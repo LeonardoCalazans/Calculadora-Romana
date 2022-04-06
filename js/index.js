ace4e6e0c6c1850833c5572bf984aae0d505d777
LucsGomes <lucasgomesribeiroskt@hotmail.com>
2022-04-06 20:29:20 -0300

    feat/adicionando conversor romano-algarismo e algorismo-romano

--- js/index.js
+++ js/index.js
@@ -64,3 +64,66 @@ function setOperation(operation) {
     return '';
 }
 
+
+function romantoAlgarism(numeroRomano){
+    let romano = numeroRomano;
+    let map = {
+        I:1,
+        V:5,
+        X:10,
+        L:50,
+        C:100,
+        D:500,
+        M:1000
+    };
+
+    let resultado = 0;
+
+    if (romano.length){
+        resultado+=map[romano[romano.length-1]];
+    }
+    for (let i= romano.length - 1; i > 0; i--){
+        if(map[romano[i-1]] < map[romano[i]]) {
+            resultado-=map[romano[i-1]];
+        }else{
+            resultado+=map[romano[i-1]];
+        }
+    }
+    console.log(`${numeroRomano} em decimal é ${resultado}`);
+}
+
+
+
+function algarismotoRoman(arabicNumber){
+
+    let numeroseNumerais = [
+        {numero: 1000, romano: 'M'}, //0
+        {numero: 900, romano: 'CM'}, //1
+        {numero: 500, romano: 'D'},  //2
+        {numero: 400, romano: 'CD'}, //3
+        {numero: 100, romano: 'C'},  //4
+        {numero: 90, romano: 'XC'},  //5
+        {numero: 50, romano: 'L'},   //6
+        {numero: 40, romano: 'XL'},  //7
+        {numero: 10, romano: 'X'},   //8
+        {numero: 9, romano: 'IX'},   //9
+        {numero: 5, romano: 'V'},    //10
+        {numero: 4, romano: 'IV'},   //11
+        {numero: 1, romano: 'I'}     //12
+    ];
+
+    let letraromana = '';
+    let numero = arabicNumber;
+    for(let i=0; i < numeroseNumerais.length; i++){
+        if(numeroseNumerais[i].numero <= numero){    // 18 - 10 = 8
+            numero = numero - numeroseNumerais[i].numero;
+                            //10        -   10
+            letraromana = letraromana + numeroseNumerais[i].romano;
+                            //''      + 'X'
+            i--
+        }
+    }
+
+    console.log(`O número ${arabicNumber} é ${letraromana} em Letra Romana`);
+
+}
